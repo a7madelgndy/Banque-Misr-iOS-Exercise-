11e8fc6138f5c43a6de941600674b53249606088
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2024-10-05 21:06:54 +0300

    first commit

---
+++ Banque Misr - iOS Exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Banque Misr - iOS Exercise
+//
+//  Created by Ahmed El Gndy on 05/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
