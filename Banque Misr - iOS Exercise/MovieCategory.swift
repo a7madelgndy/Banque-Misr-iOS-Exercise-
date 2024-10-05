3c70386157bcda1e1886a69f9c5be2a9ff571842
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2024-10-05 22:57:17 +0300

    creating nework manager class , Movie Model , and create emu MovieCategory

---
+++ Banque Misr - iOS Exercise/MovieCategory.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCategory.swift
+//  Banque Misr - iOS Exercise
+//
+//  Created by Ahmed El Gndy on 05/10/2024.
+//
+
+import Foundation
+enum MovieCategory: String {
+    case nowPlaying = "now_playing"
+    case popular = "popular"
+    case upcoming = "upcoming"
+}
